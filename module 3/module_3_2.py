03e717b47667fffa7fca43477d468af97856595d
ShlenkinVV <mr.shlenkin@mail.ru>
2024-09-07 16:24:50 +0400

    replace 'and' with 'or'

--- module 3/module_3_2.py
+++ module 3/module_3_2.py
@@ -4,7 +4,7 @@ GitHub - ShlenkinVV
 """
 
 def send_email(message, recipient, *, sender = "university.help@gmail.com"):
-    if ("@" not in recipient) or not(recipient.endswith((".com", ".ru", ".net"))) and \
+    if ("@" not in recipient) or not(recipient.endswith((".com", ".ru", ".net"))) or \
         ("@" not in sender) or not(sender.endswith((".com", ".ru", ".net"))):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
