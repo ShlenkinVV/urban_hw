bd7f18469b38101947b55ca8a44d3da9400845c0
ShlenkinVV <mr.shlenkin@mail.ru>
2024-09-15 13:21:03 +0400

    Initial commit

---
+++ module 4/module 4.1/module_4_1.py
@@ -0,0 +1,15 @@
+"""
+author - Shlenkin Vladimir
+GitHub - ShlenkinVV
+"""
+from true_math import devide as true
+from fake_math import devide as fake
+
+result1 = fake(69, 3)
+result2 = fake(3, 0)
+result3 = true(49, 7)
+result4 = true(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
