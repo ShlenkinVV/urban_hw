1e715ed33aa569b57afe46f365aa1469aff8bec7
ShlenkinVV <mr.shlenkin@mail.ru>
2024-09-27 17:40:43 +0400

    Initial commit

--- module 5/module_5_3.py
+++ module 5/module_5_3.py
@@ -2,6 +2,8 @@
 author - Shlenkin Vladimir
 GitHub - ShlenkinVV
 """
+
+
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
@@ -11,7 +13,7 @@ class House:
         if new_floor < 1 or new_floor > self.number_of_floors:
             print('Такого этажа не существует')
         else:
-            for i in range(1, new_floor+1):
+            for i in range(1, new_floor + 1):
                 print(i)
 
     def __str__(self):
@@ -66,9 +68,56 @@ class House:
     def __radd__(self, value):
         return self.__add__(value)
 
-
     def __iadd__(self, value):
         return self.__add__(value)
+
+    def __sub__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors -= value
+            return self
+        else:
+            print('Ошибка')
+
+    def __rsub__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = value - self.number_of_floors
+            return self
+        else:
+            print('Ошибка')
+
+    def __isub__(self, value):
+        return self.__sub__(value)
+
+    def __mul__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors *= value
+            return self
+        else:
+            print('Ошибка')
+
+    def __rmul__(self, value):
+        return  self.__mul__(value)
+
+    def __imul__(self, value):
+        return self.__mul__(value)
+
+    def __truediv__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors /= value
+            return self
+        else:
+            print('Ошибка')
+
+    def __rtruediv__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = value / self.number_of_floors
+            return self
+        else:
+            print('Ошибка')
+
+    def __itruediv__(self, value):
+        return self.__truediv__(value)
+
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
@@ -76,20 +125,41 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2) # __eq__
+print(h1 == h2)  # __eq__
 
-h1 = h1 + 10 # __add__
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10 # __iadd__
+h1 += 10  # __iadd__
 print(h1)
 
-h2 = 10 + h2 # __radd__
+h2 = 10 + h2  # __radd__
 print(h2)
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
+
+h1 = h1 - 1
+print(h1)
+h1 -= 1
+print(h1)
+h1 = 30 - h1
+print(h1)
+
+h1 = h1 * 3
+print(h1)
+h1 *= 2
+print(h1)
+h1 = 2 * h1
+print(h1)
+
+h2 = h2 / 3
+print(h2)
+h2 /= 2
+print(h2)
+h2 = 15 / h2
+print(h2)
