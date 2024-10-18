ce55d8d80a7c169d9e6c96d4585c4712647e25bc
ShlenkinVV <mr.shlenkin@mail.ru>
2024-10-18 18:59:27 +0400

    Initial commit

---
+++ module 7/module_7_2.py
@@ -0,0 +1,27 @@
+"""
+author - Shlenkin Vladimir
+GitHub - ShlenkinVV
+"""
+
+
+def custom_write(file_name, strings):
+    count_str = 1
+    file = open(file_name, 'w', encoding='utf-8')
+    res = {}
+    for string in strings:
+        res[(count_str, file.tell())] = string
+        file.write(string + '\n')
+        count_str +=1
+    return res
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
